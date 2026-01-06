b82dd7e4178f09b70bcbb1d93ba79595c3ceafbf
Signe <silo0003@stud.ek.dk>
2026-01-06 16:32:20 +0100

    comitting all

---
+++ js/main.js
@@ -0,0 +1,28 @@
+console.log(" JS loaded");
+
+// burgermenu
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+const links = document.querySelectorAll(".nav ul a");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("open");
+  });
+});
+
+// Alle knapper med ".backBtn"
+const backButtons = document.querySelectorAll(".backBtn");
+
+// Tilføjer click-event til hver knap
+backButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // Sender altid til projektsiden
+    window.location.href = "/projekter.html"; //Går tilbage til URL for projekter
+  });
+});
